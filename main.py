2b60b7ef29ba3fe3c3760e70848ea560309b02bc
yakshpratapsingh <79413606+yakshpratapsingh@users.noreply.github.com>
2021-02-21 21:55:38 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+#(c) Yaksh Pratap Singh
+#Do not leech without credits
+#2021
+
+
+import random
+#random is basic module provided in python which we will use to generate random strings
+
+m_error = [
+"hello",
+"hi",
+"bye",
+"jaa"
+]
+#here we defined strings as m_error
+
+
+
+ok = print(m_error)
+
+if int(1) == int (1):
+    print("Yes")
+elif int(1) == int(1):
+    print("Started")
+else:
+    random.choice(ok)
